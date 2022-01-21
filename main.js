5324326446ebeff844ba1dc6aacd2d105bbdd604
Benjamin <benjaminrowe0@gmail.com>
2022-01-21 19:50:40 +1100

    made the remake of the liker app

--- main.js
+++ main.js
@@ -4,20 +4,51 @@ const FULL_HEART = '♥'
 
 const modal = document.getElementById('modal')
 const getAllLikeTags = document.querySelectorAll('.like-glyph')
+const clickMessage = document.getElementById('clicked-heart')
 // Your JavaScript code goes here!
 document.addEventListener('DOMContentLoaded', (e) => {
   console.log('Page first loaded');
   modal.classList = "hidden"
+  clickMessage.classList = "hidden"
   addHeartsToPost()
 });
 
 function addHeartsToPost() {
   for (let index = 0; index < getAllLikeTags.length; index++) {
     const element = getAllLikeTags[index];
+    let flag = false
     element.addEventListener('click',(e) => {
-      element.classList = "activated-heart"
-      element.textContent = FULL_HEART
+      mimicServerCall()
+      .then(res => {
+        flag = !flag
+        if(flag){
+          element.classList = "activated-heart"
+          clickMessage.classList.add('clicked-heart')
+          clickMessage.classList.remove("hidden")
+          setTimeout(()=>{
+            clickMessage.classList.add('hidden')
+          },1000)
+          element.textContent = FULL_HEART
+          return
+        }
+        if(!flag) {
+          element.classList.remove('activated-heart')
+          clickMessage.classList.remove('clicked-heart')
+          element.textContent = EMPTY_HEART
+        }
+         
+      })
+      .catch(err => {
+        modal.classList.remove("hidden")
+        modal.textContent = err
+        setTimeout( () => {
+          modal.textContent = " "
+          modal.classList.add("hidden")
+        },5000)
+        console.log(`catched error ${err}`)
+      })
     })
+
   }
 }
 
